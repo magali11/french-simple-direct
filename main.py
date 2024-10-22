222526444b302cde909b64876db797a5222fdeb0
gosia gutowska <malgorzata.gutowska2@mail.dcu.ie>
2024-10-22 23:22:26 +0200

    Additional conversations

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import base64
 from pydub import AudioSegment
 import json
 
+
 ## Conf part
-conv_data_path = "conv_data/restaurant_1.json"
+# conv_data_path = "conv_data/restaurant_1.json"
 
 
 ## Functions
 
+@st.cache_data
 def text_to_speech_segment(text):
     tts = gTTS(text=text, lang='fr')
     audio_fp = BytesIO()
@@ -18,7 +20,7 @@ def text_to_speech_segment(text):
     audio_fp.seek(0)
     return AudioSegment.from_file(audio_fp, format="mp3")
 
-
+@st.cache_data
 def single_convo_segment(cnv_title, cnv_content):
     convo_segment = text_to_speech_segment(cnv_title + ".") + AudioSegment.silent(duration=1000)\
                      + text_to_speech_segment(cnv_content)
@@ -33,13 +35,11 @@ def decode_audio_segment(_audio_segment):
     return "data:audio/mp3;base64," + audio_base64
 
 
-@st.cache_data
 def single_convo(cnv_title, cnv_content):
     segment = single_convo_segment(cnv_title, cnv_content)
     return decode_audio_segment(segment)
 
 
-@st.cache_data
 def convo_playlist(_convo_segments):
     combined = AudioSegment.silent(duration=500)  # 0.5 second of silence at the start
     for segment in _convo_segments:
@@ -56,32 +56,46 @@ def load_conversation_data(conv_data_path):
 
 ## Display the streamlit page
 
-conversations = load_conversation_data(conv_data_path)
+def display_page(conversations):
+    st.write("Play all")
+    st.audio(
+        convo_playlist([single_convo_segment(convo['conv_title_fr'], convo['conv_content_fr']) for convo in conversations]),
+        format='audio/mp3'
+    )
 
-st.image("img/restaurant_1.png", use_column_width='always')
-st.header("In restaurant")
+    st.divider()
 
-st.write("Play all")
-convo_segments = [single_convo_segment(convo['conv_title_fr'], convo['conv_content_fr']) for convo in conversations]
-st.audio(convo_playlist(convo_segments), format='audio/mp3')
+    # Make a container for each conversation
+    for convo in conversations:
+        c = st.container()
+        colfr, colen = c.columns(2, gap="medium")
 
-st.divider()
+        colfr.subheader(convo['conv_title_fr'])
+        colfr.write(convo['conv_content_fr'])
 
-# Make a container for each conversation
-for convo in conversations:
-    c = st.container()
-    colfr, colen = c.columns(2, gap="medium")
+        colen.subheader(convo['conv_title_en'])
+        colen.write(convo['conv_content_en'])
 
-    colfr.subheader(convo['conv_title_fr'])
-    colfr.write(convo['conv_content_fr'])
+        audio_file = single_convo(convo['conv_title_fr'], convo['conv_content_fr'])
 
-    colen.subheader(convo['conv_title_en'])
-    colen.write(convo['conv_content_en'])
+        inc1 = c.container()
+        col11, col21 = inc1.columns([1,3], gap="small", vertical_alignment='center')
+        col11.write(":black_right_pointing_triangle_with_double_vertical_bar: Play once")
+        col21.audio(audio_file, loop=False)
 
-    audio_file = single_convo(convo['conv_title_fr'], convo['conv_content_fr'])
-    cspl, cloop = c.columns(2, gap="small")
+        inc2 = c.container()
+        col12, col22 = inc2.columns([1,3], gap="small", vertical_alignment='center')
+        col12.write(":repeat: Play continuously")
+        col22.audio(audio_file, loop=True)
 
-    cspl.write(":black_right_pointing_triangle_with_double_vertical_bar: Play once")
-    cspl.audio(audio_file, loop=False)
-    cloop.write(":repeat: Play in a loop")
-    cloop.audio(audio_file, loop=True)
+
+tab1, tab2 = st.tabs(["In restaurant", "Fussy restaurant client"])
+with tab1:
+    st.image("img/restaurant_1.png", use_column_width='always')
+    st.header("In restaurant")
+    display_page(load_conversation_data("conv_data/restaurant_1.json"))
+
+with tab2:
+    st.image("img/restaurant_2.png", use_column_width='always')
+    st.header("Fussy restaurant client")
+    display_page(load_conversation_data("conv_data/restaurant_2.json"))
