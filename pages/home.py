5ee03cd61682464b07b56a020ee03be6cc86ccad
gosia gutowska <malgorzata.gutowska2@mail.dcu.ie>
2024-10-25 17:13:15 +0200

    Brought back homepage.

--- pages/home.py
+++ pages/home.py
@@ -1,31 +1,31 @@
 import streamlit as st
 
 st.image("img/french_direct_logo.png")
-#
-# st.html('''
-#     <div style="width: 100%; padding:0.8em 1em; background-color: #044fc8; background-size: cover; background-position: center; display: flex; align-items: center; justify-content: center;">
-#         <h1 style="color: white; font-size: 36px;">Apprenez le français en direct – Parlez dès le premier jour !</h1>
-#     </div>
-# ''')
-# st.html('''
-# Laissez tomber les exercices de grammaire sans fin et plongez directement dans des
-# <span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">conversations réelles</span> !
-# Perfectionnez votre accent, apprenez un vocabulaire pratique et profitez d'une approche
-# <span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">dynamique</span>
-# et <span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">immersive</span> qui fait vivre le français.
-# Pourquoi attendre pour parler ? Commencez maintenant – faisons du français <em>votre deuxième langue</em> !
-# ''')
-#
-# st.html('''
-#     <div style="width: 100%; padding:0.8em 1em; background-color: #ff0531; background-size: cover; background-position: center; display: flex; align-items: center; justify-content: center;">
-#         <h1 style="color: white; font-size: 36px;">Learn French the Direct Way – Speak from Day One!</h1>
-#     </div>
-# ''')
-# st.html('''
-# Ditch the endless grammar drills and jump straight into
-# <span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">real-life conversations</span>!
-# Perfect your accent, learn practical vocabulary, and enjoy
-# a <span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">dynamic</span>,
-# <span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">immersive</span> approach that brings French to life.
-# Why wait to speak? Start now – let’s make French <em>votre deuxième langue</em>!
-# ''')
+
+st.html('''
+    <div style="width: 100%; padding:0.8em 1em; background-color: #044fc8; background-size: cover; background-position: center; display: flex; align-items: center; justify-content: center;">
+        <h1 style="color: white; font-size: 36px;">Apprenez le français en direct – Parlez dès le premier jour !</h1>
+    </div>
+''')
+st.html('''
+Laissez tomber les exercices de grammaire sans fin et plongez directement dans des
+<span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">conversations réelles</span> !
+Perfectionnez votre accent, apprenez un vocabulaire pratique et profitez d'une approche
+<span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">dynamique</span>
+et <span style="background-color: rgba(255, 20, 20, 0.7); padding: 0 3px 0 3px">immersive</span> qui fait vivre le français.
+Pourquoi attendre pour parler ? Commencez maintenant – faisons du français <em>votre deuxième langue</em> !
+''')
+
+st.html('''
+    <div style="width: 100%; padding:0.8em 1em; background-color: #ff0531; background-size: cover; background-position: center; display: flex; align-items: center; justify-content: center;">
+        <h1 style="color: white; font-size: 36px;">Learn French the Direct Way – Speak from Day One!</h1>
+    </div>
+''')
+st.html('''
+Ditch the endless grammar drills and jump straight into
+<span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">real-life conversations</span>!
+Perfect your accent, learn practical vocabulary, and enjoy
+a <span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">dynamic</span>,
+<span style="background-color: rgba(30, 115, 250, 0.5); padding: 0 3px 0 3px">immersive</span> approach that brings French to life.
+Why wait to speak? Start now – let’s make French <em>votre deuxième langue</em>!
+''')
